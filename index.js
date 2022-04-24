a2ffe97c4212e42746be45ff098a5d9ad4cac9ca
dirkarnez <smalldirkalex@gmail.com>
2022-04-24 19:15:14 +0800

    - upload files

--- index.js
+++ index.js
@@ -51,56 +51,84 @@ FILE "Michael Kiwanuka - Love And Hate.flac" WAVE
 `
 );
 
-parse(`REM GENRE R&B
-REM DATE 2016
-REM DISCID 720CEB0A
+parse(`REM DATE 2005
+REM DISCID 1411B112
 REM COMMENT "ExactAudioCopy v1.1"
-PERFORMER "Michael Kiwanuka"
-TITLE "Love And Hate"
-FILE "Michael Kiwanuka - Love And Hate.flac" WAVE
+PERFORMER "Schiller"
+TITLE "Day and Night"
+FILE "Schiller - Day and Night.flac" WAVE
   TRACK 01 AUDIO
-    TITLE "Cold Little Heart"
-    PERFORMER "Michael Kiwanuka"
+    TITLE "Welcome"
+    PERFORMER "Schiller"
     INDEX 01 00:00:00
   TRACK 02 AUDIO
-    TITLE "Black Man In A White World"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 00 09:58:54
-    INDEX 01 10:10:36
+    TITLE "Nightflight"
+    PERFORMER "Schiller"
+    INDEX 01 01:09:47
   TRACK 03 AUDIO
-    TITLE "Falling"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 14:29:33
+    TITLE "Tired of Being Alone"
+    PERFORMER "Schiller"
+    INDEX 01 06:55:22
   TRACK 04 AUDIO
-    TITLE "Place I Belong"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 18:46:24
+    TITLE "What's Coming"
+    PERFORMER "Schiller"
+    INDEX 01 10:57:36
   TRACK 05 AUDIO
-    TITLE "Love And Hate"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 23:33:63
+    TITLE "Sunrise"
+    PERFORMER "Schiller"
+    INDEX 01 15:23:43
   TRACK 06 AUDIO
-    TITLE "One More Night"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 30:40:70
+    TITLE "Miles and Miles"
+    PERFORMER "Schiller"
+    INDEX 01 18:36:31
   TRACK 07 AUDIO
-    TITLE "I'll Never Love"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 34:34:26
+    TITLE "Das Meer"
+    PERFORMER "Schiller"
+    INDEX 01 22:30:57
   TRACK 08 AUDIO
-    TITLE "Rule The World"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 37:20:25
+    TITLE "I Know (feat. Jette Von Roth)"
+    PERFORMER "Schiller"
+    INDEX 01 27:21:18
   TRACK 09 AUDIO
-    TITLE "Father's Child"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 01 43:03:12
-    INDEX 02 50:06:41
+    TITLE "Morning Dew"
+    PERFORMER "Schiller"
+    INDEX 01 32:19:36
   TRACK 10 AUDIO
-    TITLE "The Final Frame"
-    PERFORMER "Michael Kiwanuka"
-    INDEX 00 50:06:41
-    INDEX 01 50:08:23
+    TITLE "Berlin Bombay"
+    PERFORMER "Schiller"
+    INDEX 01 36:22:07
+  TRACK 11 AUDIO
+    TITLE "The Smile"
+    PERFORMER "Schiller"
+    INDEX 01 41:11:63
+  TRACK 12 AUDIO
+    TITLE "Lightstorm (feat. Kim Sanders)"
+    PERFORMER "Schiller"
+    INDEX 01 44:26:20
+  TRACK 13 AUDIO
+    TITLE "Falling"
+    PERFORMER "Schiller"
+    INDEX 01 47:45:33
+  TRACK 14 AUDIO
+    TITLE "Rings of Time"
+    PERFORMER "Schiller"
+    INDEX 01 53:02:26
+  TRACK 15 AUDIO
+    TITLE "I Saved You (feat. Kim Sanders)"
+    PERFORMER "Schiller"
+    INDEX 01 56:52:66
+  TRACK 16 AUDIO
+    TITLE "Misguiding Light"
+    PERFORMER "Schiller"
+    INDEX 01 61:31:43
+  TRACK 17 AUDIO
+    TITLE "Fireworks"
+    PERFORMER "Schiller"
+    INDEX 01 64:49:60
+  TRACK 18 AUDIO
+    TITLE "Sleepy Storm (feat. Jette Von Roth)"
+    PERFORMER "Schiller"
+    INDEX 01 69:54:21
 `
 );
 
@@ -115,7 +143,7 @@ function parse(content) {
         const line = lines[i];
         
         if (line.startsWith('FILE')) {
-            const result = /^FILE\s+"([^\\s]+)"\s+([^\s]+)$/i.exec(line);
+            const result = /^FILE\s+"([^\\"]+)"\s+([^\s]+)$/i.exec(line);
             data.fileName = result[1];
             data.fileFormat = result[2];
         } else if (line.startsWith('TRACK')) {
